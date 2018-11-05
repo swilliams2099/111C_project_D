5991364b019c3fbd6cadb2855237cfeb13696d48
S <swilliams2099@gmail.com>
2018-11-04 19:32:31 -0800

    added comments, ListHeadTailList

--- src/LinkedHeadTailList.java
+++ src/LinkedHeadTailList.java
@@ -5,65 +5,87 @@ public class LinkedHeadTailList<T> implements HeadTailListInterface<T> {
 
 	private Node head,tail;
 
+	// WY
+	/**
+	 * @param head
+	 * @param tail
+	 */
+	public LinkedHeadTailList(LinkedHeadTailList<T>.Node head, LinkedHeadTailList<T>.Node tail) {
+		super();
+		this.head = head;
+		this.tail = tail;
+	}
+
+	// WM
 	@Override
 	public void addFront(T newEntry) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	// SW
 	@Override
 	public void addBack(T newEntry) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	// WY
 	@Override
 	public T removeFront() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	// SW
 	@Override
 	public T removeBack() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	// WY
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	// WY
 	@Override
 	public T getEntry(int givenPosition) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	// SW
 	@Override
 	public void display() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	// WM
 	@Override
 	public int contains(T anEntry) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	// SW
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	// WM
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	
 	private class Node {
 		private T data; // Entry in list
 		private Node next; // Link to next node
