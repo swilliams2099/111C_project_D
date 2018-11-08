37429994e2e7801b3188fc20307659bdfb8de129
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2018-11-08 15:44:23 -0800

    first pass code done, needs debug run

--- src/ListHeadTailList.java
+++ src/ListHeadTailList.java
@@ -1,66 +1,92 @@
+import java.util.List;
 
 public class ListHeadTailList<T> implements HeadTailListInterface<T> {
-	
-	List<T> list; // initialize to type ArrayList or LinkedList
+
+	List<T> list = new ArrayList<T>; // initialize to type ArrayList or LinkedList
+	int head = 0;
+	int tail = list.size() - 1;
 
 	@Override
 	public void addFront(T newEntry) {
-		// TODO Auto-generated method stub
-		
+		for (int i = tail; i > 0; i--) {
+			list[i + 1] = list[i];
+		}
+		tail++;
+		list[0] = newEntry;
 	}
 
 	@Override
 	public void addBack(T newEntry) {
-		// TODO Auto-generated method stub
-		
+		list[tail + 1] = newEntry;
+		tail++;
+
 	}
 
 	@Override
 	public T removeFront() {
-		// TODO Auto-generated method stub
-		return null;
+		T returned = list[0];
+		for (int i = 1; i <= list.size(); i++) {
+			list[i - 1] = list[i];
+		}
+		list[tail] = null;
+		tail--;
+		return returned;
 	}
 
 	@Override
 	public T removeBack() {
-		// TODO Auto-generated method stub
-		return null;
+		T returned = list[list.size() - 1];
+		list[list.size() - 1] = null;
+		return returned;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		list = new ArrayList<T>;
+		head = 0;
+		tail = list.size();
+
 	}
 
 	@Override
 	public T getEntry(int givenPosition) {
-		// TODO Auto-generated method stub
-		return null;
+		T returned = list[givenPosition];
+		return returned;
 	}
 
 	@Override
 	public void display() {
-		// TODO Auto-generated method stub
-		
+		System.out.println(list.toString());
+
 	}
 
 	@Override
 	public int contains(T anEntry) {
-		// TODO Auto-generated method stub
-		return 0;
+		boolean found = false
+	    int index = -1;
+		boolean loopDone = false;
+		while(!found && !loopDone) {
+			for(int i = 0; i < list.size(); i++) {
+				if(list[i].equals(anEntry)) {
+					index = i;
+					found = true;
+				}
+				if(i == list.size() - 1) {
+					loopDone = true;
+				}
+			}
+		}
+		return index;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return list.size();
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return list.size() == 0;
 	}
-	
+
 }
